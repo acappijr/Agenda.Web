2fa9a3bec15ea08fc29a14bcd906b8d3c8d4171d
Amauricio Cappi Junior <acappijr@yahoo.com.br>
2023-10-18 13:59:09 -0300

    Ajustes finais e Readme

--- src/Application/Models/ErrorViewModel.cs
+++ src/Application/Models/ErrorViewModel.cs
@@ -1,12 +1,7 @@
-namespace Agenda.Application.Models
+namespace Agenda.Application.Models;
+public class ErrorViewModel
 {
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
+    public string? RequestId { get; set; }
 
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
-
-
-// https://www.red-gate.com/simple-talk/databases/sql-server/learn/when-use-char-varchar-varcharmax/#:~:text=The%20fundamental%20difference%20between%20CHAR,both%20can%20store%20alphanumeric%20data.
